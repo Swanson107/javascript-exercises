e34e0a82ae60474bcd7511e4c0a011de54a2153a
Thomas S <thomas.swanson107@gmail.com>
2023-03-29 15:31:28 -0700

    Finished 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (Number.isInteger(num1) && Number.isInteger(num2) && num1 > 0 && num2 > 0){
+        if (num1 < num2){
+            for (let i = num1; i <= num2; i++){
+                num1 += i;
+            }
+            return num1 - 1;
+        } else {
+            for (let i = num2; i <= num1; i++){
+                num2 += i;
+            }
+            return num2 - 1;
+        }
+    } else {
+        return "ERROR";
+    }
 
 };
 
