8586d3138fc8e7f886b8bf73c942b53e1d4eeeb0
Thomas S <thomas.swanson107@gmail.com>
2023-03-29 16:57:18 -0700

    Finished 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempf) {
+  let tempc = (tempf - 32) * 5/9;
+  newTemp = parseFloat(tempc).toFixed(1);
+  newTemp = parseFloat(newTemp);
+  return newTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempc) {
+  let tempf = (tempc * 9/5) + 32;
+  newTemp = parseFloat(tempf).toFixed(1);
+  newTemp = parseFloat(newTemp);
+  return newTemp;
 };
 
 // Do not edit below this line
