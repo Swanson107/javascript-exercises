eae4805c4c65d9ba75483d3d64a9c5c5982e9c8e
Thomas S <thomas.swanson107@gmail.com>
2023-03-29 15:13:22 -0700

    Finished 3 and 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(nums, ...args) {
+    let newNums = [];
+    for (i = 0; i < nums.length; i++){
+        if (!args.includes(nums[i])){
+            newNums.push(nums[i]);
+        }
+    }
+    return newNums;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
