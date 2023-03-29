efdf35bc78077aab8aa92aae539f33e0542bf3c9
Thomas S <thomas.swanson107@gmail.com>
2023-03-29 13:22:22 -0700

    Finish first 2 exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    if (times < 0) {
+        return "ERROR";
+    }
+    let answer = '';
+    for (let i = 0; i < times; i++){
+        answer += word;
+    }
+    return answer;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
