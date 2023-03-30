18e5086b8f41087a10b57cd7f4a7c92c0ec099d5
Thomas S <thomas.swanson107@gmail.com>
2023-03-29 19:21:42 -0700

    Finished 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.toLowerCase().replace(/[^a-z]/g, "");
+    let array1 = string.split("");
+    let array2 = array1.reverse();
+    newString = array2.join("");
+    if (newString.split("").reverse().join("") == newString){
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
