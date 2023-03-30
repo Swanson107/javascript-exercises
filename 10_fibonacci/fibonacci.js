5fe513705a88e7c9876217836bf10375fd19c998
Thomas S <thomas.swanson107@gmail.com>
2023-03-29 20:03:21 -0700

    Finished 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0){return "OOPS"}
+    num = parseInt(num);
+    let n1 = 0;
+    let n2 = 1;
+    let fib = [1];
+    for (let i = 0; i <= num; i++){
+        result = n1 + n2;
+        fib.push(result);
+        n1 = n2;
+        n2 = result;
 
+    }
+    result = fib[num - 1];
+    return result;
 };
 
 // Do not edit below this line
